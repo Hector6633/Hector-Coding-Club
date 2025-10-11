bac8ae184fc418bfb2640e17bc4ce041ef4e39b8
Rajeev <rajeevlakshmanan003@gmail.com>
2025-10-11 23:25:35 +0530

    Customer registration function implemented.

--- themes/admin.py
+++ themes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from . models import Registrations
 
-# Register your models here.
+admin.site.register(Registrations)
--- themes/views.py
+++ themes/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Registrations
+from django.contrib.messages import success, error
 
-# Create your views here.
 def index(request):
+    if request.method == 'POST':
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            client_data = Registrations.objects.create(name=name, email=email, mobile_number=phone, message=message)
+            client_data.save()
+            success_msg = 'Successfully Registered'
+            success(request, success_msg)
+            return redirect('index')
+        except Exception as e:
+            error_msg = 'Error While Registering'
+            error(request, error_msg)
+            return redirect('index')
     return render(request, 'index.html')
